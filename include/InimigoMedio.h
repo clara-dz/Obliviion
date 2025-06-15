5c79e61fbc46bffaa45b448b4054fa5a6645b6ed
clara-dz <mariadiniz.work@gmail.com>
2025-06-15 14:45:05 -0300

    added InimigoMedio.h

---
+++ include/InimigoMedio.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Inimigo.h"
+
+class InimigoMedio : public Inimigo {
+private:
+    int tamanho;
+
+public:
+    // Construtor que recebe os parâmetros iniciais e a textura
+    InimigoMedio(const sf::Texture& texture, int x = 0, int y = 0, int speed = 0);
+    ~InimigoMedio();
+
+    // Implementação dos métodos virtuais puros herdados de Inimigo e Entidade
+    void update(float deltaTime) override;
+    void handleCollision(Entidade& other) override;
+    // void danificar(Jogador* pJogador) override;
+    // void salva() override;
+};
