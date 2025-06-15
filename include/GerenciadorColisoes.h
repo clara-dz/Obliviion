64f3939803ecaa43e0afd986576bee5085da13af
clara-dz <mariadiniz.work@gmail.com>
2025-06-15 14:29:35 -0300

    added Obstaculo.h and static to GerenciadorDeColisoes.h

--- include/GerenciadorColisoes.h
+++ include/GerenciadorColisoes.h
@@ -1,10 +1,24 @@
 #pragma once
 
 #include <vector>
+#include <list>
+#include <set>
+
 #include "Jogador.h"
 #include "Floor.h"
+#include "Inimigo.h"
+#include "Obstaculo.h"
+#include "Projetil.h"
 
 class GerenciadorColisoes {
+static GerenciadorColisoes* instancia;
+    Jogador* pJog1;
+    Jogador* pJog2;
+    std::vector<Inimigo*>   listaInimigos;
+    std::list<Obstaculo*>   listaObstaculos;
+    std::set<Projetil*>     listaProjeteis;
+    // GerenciadorColisoes();
+
 public:
     void checarColisoes(Personagem& Personagem, const Floor& floor);
     void checarColisaoEntrePersonagens(Personagem& p1, Personagem& p2);
---
+++ include/Obstaculo.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Entidade.h"
+
+
+// TODO: Descobrir como colocar em um namespace
+class Obstaculo : public Entidade {
+protected:
+    bool danoso; // Flag para indicar se o obstáculo causa dano
+public:
+    Obstaculo();
+    virtual ~Obstaculo();
+    // Métodos virtuais puros que as classes filhas DEVERÃO implementar.
+    // Note que estamos usando os nomes da nossa arquitetura base.
+    virtual void update(float deltaTime) override = 0;
+    virtual void handleCollision(Entidade& other) override = 0;
+    virtual void salva() = 0; // Para o sistema de save futuro
+};
