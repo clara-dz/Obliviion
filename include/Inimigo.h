2c5eb8a5942aea80aab543e9d5551e1f385df6ea
clara-dz <mariadiniz.work@gmail.com>
2025-06-15 14:42:54 -0300

    added comments to Inimigo.h

--- include/Inimigo.h
+++ include/Inimigo.h
@@ -2,8 +2,6 @@
 #include "Personagem.h"
 #include "Jogador.h"
 
-#pragma once
-#include "Personagem.h"
 
 class Inimigo : public Personagem {
     public:
@@ -11,5 +9,11 @@ class Inimigo : public Personagem {
         Inimigo(int x, int y, int speed) : Personagem(x, y, speed) {}
         virtual ~Inimigo() = default;
     
-        virtual void atacar(Jogador& jogador) = 0; // Abstract method to be implemented by subclasses
+        virtual void atacar(Jogador& jogador) = 0;
+
+        // Lógica de ataque do inimigo. Substitui o antigo 'atacar'. Precisa mudar no .cpp
+        // virtual void danificar(Jogador* pJogador) = 0; 
+    
+        // Função para salvar o estado (será implementada no futuro). Descobrir pq dá erro
+        // virtual void salva() = 0;
 };
