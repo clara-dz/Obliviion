83725a4d6981bff92a5c5c3135759f0598429d31
clara-dz <mariadiniz.work@gmail.com>
2025-06-15 14:20:02 -0300

    added classes: Chefao.h and Projetil.h

---
+++ include/Projetil.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Entidade.h"
+
+class Projetil : public Entidade {
+protected:
+    sf::Vector2f velocidade;
+    bool ativo;
+
+public:
+    // O construtor precisa de uma textura para o sprite do projétil
+    Projetil(const sf::Texture& texture);
+    ~Projetil();
+
+    // Ativa o projétil, definindo sua posição e velocidade iniciais
+    void disparar(sf::Vector2f pos, sf::Vector2f vel);
+
+    // Verifica se o projétil está atualmente em movimento
+    bool estaAtivo() const;
+
+    // Métodos virtuais herdados de Entidade que PRECISAM ser implementados
+    void update(float deltaTime) override;
+    void handleCollision(Entidade& other) override;
+    
+    // Vamos sobrescrever o render para desenhar apenas se estiver ativo
+    void render(sf::RenderWindow& window) override;
+};
