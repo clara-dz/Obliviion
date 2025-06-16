bc5ac02f128e7835cfe2adf792678fc68ec0ff21
clara-dz <mariadiniz.work@gmail.com>
2025-06-16 00:14:44 -0300

    Inimigo

--- include/Inimigo.h
+++ include/Inimigo.h
@@ -2,18 +2,20 @@
 #include "Personagem.h"
 #include "Jogador.h"
 
-
 class Inimigo : public Personagem {
+    protected:
+        int nivelMaldade;
     public:
+        void salvarDataBuffer();
+        // virtual void danificar(Jogador* p) = 0;
         Inimigo() : Personagem() {}
         Inimigo(int x, int y, int speed) : Personagem(x, y, speed) {}
         virtual ~Inimigo() = default;
-        // void salvar() override;
+        // virtual void salvar() = 0;
     
         virtual void atacar(Jogador& jogador) = 0;
 
         // Lógica de ataque do inimigo. Substitui o antigo 'atacar'. Precisa mudar no .cpp
-        // virtual void danificar(Jogador* pJogador) = 0; 
         virtual void executar(float deltaTime) = 0;
         // Função para salvar o estado (será implementada no futuro). Descobrir pq dá erro
         // virtual void salva() = 0;
