de557f146088869ba5369064cca752fa28ab5323
clara-dz <mariadiniz.work@gmail.com>
2025-06-15 23:47:19 -0300

    added desenharEnte to gerenciadorGrafico

--- include/GerenciadorGrafico.h
+++ include/GerenciadorGrafico.h
@@ -7,35 +7,36 @@ class Ente;
 namespace Gerenciadores {
 
     class GerenciadorGrafico {
-    private:
-        // Ponteiro para a janela principal do jogo.
-        sf::RenderWindow* window;
+        private:
+            // Ponteiro para a janela principal do jogo.
+            sf::RenderWindow* window;
 
-        // A única instância da classe (padrão Singleton).
-        static GerenciadorGrafico* instancia;
+            // A única instância da classe (padrão Singleton).
+            static GerenciadorGrafico* instancia;
 
-        // Construtor privado para garantir que apenas um objeto seja criado.
-        GerenciadorGrafico();
+            // Construtor privado para garantir que apenas um objeto seja criado.
+            GerenciadorGrafico();
 
-    public:
-        ~GerenciadorGrafico();
+        public:
+            ~GerenciadorGrafico();
 
-        // Método de acesso estático para obter a instância única.
-        static GerenciadorGrafico* getInstance();
+            // Método de acesso estático para obter a instância única.
+            static GerenciadorGrafico* getInstance();
 
-        // Apagamos o construtor de cópia e o operador de atribuição
-        // para garantir que a instância não possa ser clonada.
-        GerenciadorGrafico(const GerenciadorGrafico&) = delete;
-        GerenciadorGrafico& operator=(const GerenciadorGrafico&) = delete;
+            // Apagamos o construtor de cópia e o operador de atribuição
+            // para garantir que a instância não possa ser clonada.
+            GerenciadorGrafico(const GerenciadorGrafico&) = delete;
+            GerenciadorGrafico& operator=(const GerenciadorGrafico&) = delete;
 
-        // Retorna um ponteiro para a janela, para que outros objetos possam desenhar nela.
-        sf::RenderWindow* getJanela();
+            // Retorna um ponteiro para a janela, para que outros objetos possam desenhar nela.
+            sf::RenderWindow* getJanela();
 
-        // Funções de controle da janela
-        void limparTela();
-        void mostrarTela();
-        bool janelaEstaAberta() const;
-        void fecharJanela();
-    };
+            // Funções de controle da janela
+            void limparTela();
+            void mostrarTela();
+            bool janelaEstaAberta() const;
+            void fecharJanela();
+            void desenharEnte(Ente* pE);
+        };
 
 } 
