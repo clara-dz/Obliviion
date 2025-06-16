70c3ecd0a5ca7099b7004218227f942b092136a3
clara-dz <mariadiniz.work@gmail.com>
2025-06-16 00:24:44 -0300

    Menu

--- include/Menu.h
+++ include/Menu.h
@@ -4,30 +4,32 @@
 #include <string>
 #include "Ente.h"
 
+class Jogo;
 class Menu : public Ente {
-private:
-    std::vector<std::string> items;
-    std::vector<sf::Text> texts;
-    sf::Font font;
-    std::size_t selectedIndex;
-    bool isOpen;
+    private:
+        Jogo* pJog;
+        std::vector<std::string> items;
+        std::vector<sf::Text> texts;
+        sf::Font font;
+        std::size_t selectedIndex;
+        bool isOpen;
 
-    sf::RectangleShape backgroundBox;
+        sf::RectangleShape backgroundBox;
 
-public:
-    Menu(const std::vector<std::string>& itemLabels, const sf::Font& font);
+    public:
+        Menu(const std::vector<std::string>& itemLabels, const sf::Font& font);
 
-    void open();
-    void close();
-    bool isOpened() const;
+        void open();
+        void close();
+        bool isOpened() const;
 
-    void handleEvent(const sf::Event& event, sf::RenderWindow& window);
-    
-    // Required overrides from Ente
-    void executar(float deltaTime) override;
-    void render(sf::RenderWindow& window) override;
-    void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+        void handleEvent(const sf::Event& event, sf::RenderWindow& window);
 
-private:
-    void updateVisual();
+        // Required overrides from Ente
+        void executar(float deltaTime) override;
+        void render(sf::RenderWindow& window) override;
+        void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
+    private:
+        void updateVisual();
 };
