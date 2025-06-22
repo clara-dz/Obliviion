f4ff2a9c01e9571e9b8116100dcef58a2accbf41
leobettio <leoscharf.bettio@gmail.com>
2025-06-22 00:33:31 -0300

    incluiu cpp chama de hades

---
+++ include/ChamaDeHades.cpp
@@ -0,0 +1,18 @@
+#include "ChamaDeHades.h"
+
+ChamaDeHades::ChamaDeHades(const sf::Texture& texture, float x, float y) {
+    sprite.setTexture(texture);
+    sprite.setPosition(x, y);
+}
+
+void ChamaDeHades::obstaculizar(Jogador& jogador) {
+    float knockbackSpeed = 500.f;
+
+    jogador.setVelocityX(-1 * knockbackSpeed*danosidade);
+    jogador.setVelocityY(-15.f);
+    jogador.setKnockbackTimer(30.f);
+}
+
+void ChamaDeHades::executar(float deltaTime) {}
+
+
